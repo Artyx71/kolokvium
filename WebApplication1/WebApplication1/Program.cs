9c6251d6058e3f879075a6c290453948f0664c53
Andrew Gabaraev <46446380+Artyx71@users.noreply.github.com>
2023-01-09 12:54:34 +0300

    Add files via upload

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -7,19 +7,41 @@ app.MapGet("/", async (context) =>
     await context.Response.SendFileAsync("html/index.html");
 });
 
-app.MapGet("/byte/{kylobyte}", ByteConvert);
-app.MapGet("/know/{s}/{r}/{k}", ByteConvert);
+app.MapGet("/byte/{bite}", ByteConvert);
+app.MapGet("/convertMoney/{money}", MoneyRequest);
 app.Run();
 
 
-string ByteConvert(string byte)
+string ByteConvert(string bite)
 {
-
-    string kylobyte = Convert.ToString(int.Parse(byte) * 1024);
-    return $"Байтов: {byte}, Килобайтов: {kylobyte}";
+    StreamWriter sw = new StreamWriter("./log.txt", true);
+    string kylobyte = Convert.ToString(int.Parse(bite) / 1024);
+    string megabyte = Convert.ToString(int.Parse(kylobyte) / 1024);
+    sw.WriteLine($"Кол-во Байтов:{bite} Килобайтов: {kylobyte}, Мегабайтов: {megabyte}");
+    sw.Close();
+    return $"Килобайтов: {kylobyte}, Мегабайтов: {megabyte}";
 }
 
-string KnowRequest(string s, string r, string k)
+string MoneyRequest(string money)
 {
-    return "kkk";
+    StreamWriter sw = new StreamWriter("./log.txt", true);
+
+    int M = int.Parse(money);
+
+    int A = M % 500;
+
+    int A1 = M / 500;
+
+    int B = A % 100;
+
+    int B1 = A / 100;
+
+    int C = B % 10;
+
+    int C1 = B / 10;
+
+    int D1 = C / 2;
+    sw.WriteLine($"Деньги:{M},500: {A1}, 100: {B1}, 10: {C1}, 2: {D1}");
+    sw.Close();
+    return $"500: {A1}, 100: {B1}, 10: {C1}, 2: {D1}";
 }
